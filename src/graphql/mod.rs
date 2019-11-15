87ad712e14843c19cd291567c63b9af4eff5efd2
Brandon Williams <bmwill@fb.com>
2019-11-15 15:59:54 -0800

    graphql: add github schema and sample queries

---
+++ src/graphql/mod.rs
@@ -0,0 +1,7 @@
+/// GraphQL queries and mutations for Github's v4 GraphQL API
+///
+/// `github-schema.graphql` is sourced from
+/// [here](https://github.com/octokit/graphql-schema/blob/master/schema.graphql)
+///
+/// [Github's v4 API Explorer](https://developer.github.com/v4/explorer/)
+/// [Github's v4 API Docs](https://developer.github.com/v4/)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod graphql;
+
 #[cfg(test)]
 mod tests {
     #[test]
