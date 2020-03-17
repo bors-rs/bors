88d7131eca7038b5426edf089a7cb1d57b86882e
Jack Moffitt <metajack@fb.com>
2020-03-17 17:20:23 -0500

    Update deps

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,10 @@
 use crate::Error;
-use sled::Db;
 use std::path::Path;
 
 pub struct Database(sled::Db);
 
 impl Database {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
-        Ok(Database(Db::open(path)?))
+        Ok(Database(sled::open(path)?))
     }
 }
