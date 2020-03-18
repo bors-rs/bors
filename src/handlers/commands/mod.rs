c466927dbc7e146a8f809e5f32026c816a2e438c
Jack Moffitt <metajack@fb.com>
2020-03-18 16:37:33 -0500

    Add initial commands shimming

--- src/handlers/commands/mod.rs
+++ src/handlers/commands/mod.rs
@@ -1,14 +1,27 @@
-use crate::github::Webhook;
+use crate::github::{Comment, Event, EventType, IssueCommentEvent, PullRequestReviewEvent, Review, Webhook};
 use crate::handlers::Handler;
+use log::info;
 
 #[derive(Debug)]
 pub struct CommandHandler;
 
 impl Handler for CommandHandler {
-    fn route(&self, _webhook: &Webhook) -> bool {
-        false
+    fn route(&self, webhook: &Webhook) -> bool {
+        matches!(
+            webhook.event_type,
+            EventType::IssueComment | EventType::PullRequestReview
+        )
     }
 
-    fn handle(&self, _webhook: &Webhook) {
+    fn handle(&self, webhook: &Webhook) {
+        let body = match &webhook.event {
+            Event::IssueComment(IssueCommentEvent { comment: Comment { ref body, .. }, .. }) => body,
+            Event::PullRequestReview(PullRequestReviewEvent { review: Review { ref body, .. }, .. }) => body,
+            _ => &None,
+        };
+
+        if let Some(ref body) = body {
+            info!("comment body = \n{}", body);
+        }
     }
 }
