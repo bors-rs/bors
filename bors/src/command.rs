66b0817d9f1ea9580f680c529284bcd924c2a3e8
Brandon Williams <bmwill@fb.com>
2020-04-20 10:25:29 -0700

    bors: add initial command parsing logic

--- bors/src/command.rs
+++ bors/src/command.rs
@@ -1,32 +1,191 @@
 //! Defines commands which can be asked to be performed
-use std::str::FromStr;
 use thiserror::Error;
+
+#[derive(Error, Debug)]
+#[error("invalid command")]
+pub struct ParseCommnadError;
 
 #[derive(Debug)]
 pub struct Command {
     cmd: String,
+    command_type: CommandType,
+}
+
+#[derive(Debug)]
+enum CommandType {
+    Approve(Approve),
+    Unapprove,
+    Land(Land),
+    Retry(Retry),
+    Cancel,
+    Priority(Priority),
+}
+
+#[derive(Debug)]
+struct Approve {
+    priority: Option<Priority>,
+}
+
+impl Approve {
+    fn with_args<'a, I>(iter: I) -> Result<Self, ParseCommnadError>
+    where
+        I: IntoIterator<Item = (&'a str, Option<&'a str>)>,
+    {
+        let mut priority = None;
+
+        for (key, value) in iter {
+            match key {
+                "p" | "priority" => {
+                    priority = Some(Priority::from_arg(value)?);
+                }
+
+                // First key we hit that we don't understand we should just bail
+                _ => break,
+            }
+        }
+
+        Ok(Self { priority })
+    }
+}
+
+#[derive(Debug)]
+struct Land {
+    priority: Option<Priority>,
+}
+
+impl Land {
+    fn with_args<'a, I>(iter: I) -> Result<Self, ParseCommnadError>
+    where
+        I: IntoIterator<Item = (&'a str, Option<&'a str>)>,
+    {
+        let mut priority = None;
+
+        for (key, value) in iter {
+            match key {
+                "p" | "priority" => {
+                    priority = Some(Priority::from_arg(value)?);
+                }
+
+                // First key we hit that we don't understand we should just bail
+                _ => break,
+            }
+        }
+
+        Ok(Self { priority })
+    }
+}
+
+#[derive(Debug)]
+struct Retry {
+    priority: Option<Priority>,
+}
+
+impl Retry {
+    fn with_args<'a, I>(iter: I) -> Result<Self, ParseCommnadError>
+    where
+        I: IntoIterator<Item = (&'a str, Option<&'a str>)>,
+    {
+        let mut priority = None;
+
+        for (key, value) in iter {
+            match key {
+                "p" | "priority" => {
+                    priority = Some(Priority::from_arg(value)?);
+                }
+
+                // First key we hit that we don't understand we should just bail
+                _ => break,
+            }
+        }
+
+        Ok(Self { priority })
+    }
+}
+
+#[derive(Debug)]
+struct Priority {
+    priority: u32,
+}
+
+impl Priority {
+    fn with_args<'a, I>(iter: I) -> Result<Self, ParseCommnadError>
+    where
+        I: IntoIterator<Item = (&'a str, Option<&'a str>)>,
+    {
+        let mut iter = iter.into_iter();
+
+        let arg = iter
+            .next()
+            .and_then(|(k, v)| if v.is_some() { None } else { Some(k) });
+
+        Self::from_arg(arg)
+    }
+
+    fn from_arg(value: Option<&str>) -> Result<Self, ParseCommnadError> {
+        if let Some(v) = value {
+            //TODO better error message (expected integer)
+            let priority = v.parse().map_err(|_| ParseCommnadError)?;
+            Ok(Self { priority })
+        } else {
+            // No value specified
+            //TODO better error message
+            return Err(ParseCommnadError);
+        }
+    }
 }
 
 impl Command {
     pub fn from_comment(c: &str) -> Option<Result<Self, ParseCommnadError>> {
         c.lines()
             .find(|line| line.starts_with('/'))
-            .map(FromStr::from_str)
+            .map(Self::from_line)
     }
-}
 
-#[derive(Error, Debug)]
-#[error("invalid command")]
-pub struct ParseCommnadError;
-
-impl FromStr for Command {
-    type Err = ParseCommnadError;
-
-    // TODO extend this impl to handle more commands and parameters
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "/lgtm" | "/land" => Ok(Command { cmd: s.to_owned() }),
-            _ => Err(ParseCommnadError),
+    fn from_line(s: &str) -> Result<Self, ParseCommnadError> {
+        if !s.starts_with('/') {
+            return Err(ParseCommnadError);
         }
+
+        let command_type = Self::from_iter(s[1..].split_whitespace())?;
+
+        Ok(Command {
+            cmd: s.to_owned(),
+            command_type,
+        })
+    }
+
+    fn from_iter<'a, I>(iter: I) -> Result<CommandType, ParseCommnadError>
+    where
+        I: IntoIterator<Item = &'a str>,
+    {
+        let mut iter = iter.into_iter();
+
+        let command_name = if let Some(name) = iter.next() {
+            name
+        } else {
+            return Err(ParseCommnadError);
+        };
+
+        // Arguments take the form of `<key>=<value>`
+        let args = iter.map(|arg| {
+            if let Some(idx) = arg.find('=') {
+                (&arg[..idx], Some(&arg[idx + 1..]))
+            } else {
+                (arg, None)
+            }
+        });
+
+        let command_type = match command_name {
+            "approve" | "lgtm" | "r+" => CommandType::Approve(Approve::with_args(args)?),
+            "unapprove" | "r-" => CommandType::Unapprove,
+            "land" => CommandType::Land(Land::with_args(args)?),
+            "retry" => CommandType::Retry(Retry::with_args(args)?),
+            "cancel" | "stop" => CommandType::Cancel,
+            "priority" => CommandType::Priority(Priority::with_args(args)?),
+
+            _ => return Err(ParseCommnadError),
+        };
+
+        Ok(command_type)
     }
 }
